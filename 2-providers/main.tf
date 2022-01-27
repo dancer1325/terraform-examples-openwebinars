a653a467347d2c9f49dd7556ceed5d0ab512a6f1
dancer1325 <alfredotic0809@gmail.com>
2022-01-27 16:18:55 +0100

    Add commentaries

--- 2-providers/main.tf
+++ 2-providers/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.10.7"
+  required_version = ">= 0.10.7" // Good practise to indicate the Terraform version to use
 }
 
 provider "aws" {
-  region = "eu-west-1"
-  allowed_account_ids = ["723002569774"]
-  profile = "openwebinars"
+  region = "eu-central-1"
+  allowed_account_ids = ["723002569774"] // AWS account allowed to create resources -- NOT to commit in a public repo
+  profile = "default" // Profile created and stored in the aws folder on premise
 }
