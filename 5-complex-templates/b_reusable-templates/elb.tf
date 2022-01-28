ae3fd4c804c666f27bc4589d1ff4746ea62cbc69
dancer1325 <alfredotic0809@gmail.com>
2022-01-28 17:40:38 +0100

    Fix issues for new releases in aws_elb

--- 5-complex-templates/b_reusable-templates/elb.tf
+++ 5-complex-templates/b_reusable-templates/elb.tf
@@ -1,14 +1,20 @@
 data "aws_availability_zones" "az" {}
 
 resource "aws_elb" "elb-web" {
-  name_prefix = "${var.project}-"
+  // Mandatory fields to fill out to create an aws_elb
+  // 1) name_prefix
+#  name_prefix = "${var.project}-" // Defined the prefix name of the load balancer. Maximum lenght is 6
+  // 2) name
+  name = "${var.environment}-${var.project}" // Defined the name of the load balancer
 
-  "listener" {
+#  "listener" {  // Deprecated
+    listener {
     instance_port = 50
     instance_protocol = "http"
     lb_port = 80
     lb_protocol = "http"
   }
 
-  availability_zones = ["${data.aws_availability_zones.az.names}"]
+#  availability_zones = ["${data.aws_availability_zones.az.names}"] // Deprecated
+  availability_zones = data.aws_availability_zones.az.names
 }
