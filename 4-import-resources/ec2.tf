f40caecdf78dd095cf1e5bac934bcf3cb905bba7
dancer1325 <alfredotic0809@gmail.com>
2022-01-28 16:14:06 +0100

    Add commentaries

--- 4-import-resources/ec2.tf
+++ 4-import-resources/ec2.tf
@@ -1,7 +1,10 @@
+// Declare the resource which is going to be imported
 resource "aws_instance" "web" {
-  ami = "ami-acd005d5"
-  instance_type = "t2.small"
-  tags {
-    Name = "openwebinars-webserver"
-  }
+#  ami = "ami-acd005d5" // Old ami version
+  ami = "ami-0dded1c9a19d0b092"
+#  instance_type = "t2.small"  // Old instance type created via aws console
+  instance_type = "t2.micro"
+#  tags = {
+#    Name = "openwebinars-webserver"
+#  }
 }
