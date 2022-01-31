d543fee380fa7836e77818700c48d87e8cec3386
dancer1325 <alfredotic0809@gmail.com>
2022-01-31 15:12:25 +0100

    Add docker-compose.yml

--- 7-other-providers/a_mysql/main.tf
+++ 7-other-providers/a_mysql/main.tf
@@ -2,7 +2,21 @@ provider "aws" {
   region              = "${var.aws_region}"
   allowed_account_ids = ["${var.aws_id}"]
   profile = "default"
-  version = "~> 1.0"
+#  version = "~> 1.0"
+}
+
+// Known bug in terraform
+// No possible to define a provider which depends on variables to another provider -->
+// 1) Comment all part of the code related to the provider, 2) Run terraform plan, terraform apply, 3) Uncomment
+
+// Required from 0.13 terraform version to use providers no Hashicorp official
+terraform {
+  required_providers {
+    mysql = {
+      source = "winebarrel/mysql"
+      version = "1.10.6"
+    }
+  }
 }
 
 // MySQL provider configured based on the outcome of creating the aws_db_instance
@@ -10,4 +24,5 @@ provider "mysql" {
   endpoint = "${aws_db_instance.mydb.endpoint}"
   username = "${var.rds_username}"
   password = "${var.rds_passwd}"
+#  version = "1.10.6"
 }
